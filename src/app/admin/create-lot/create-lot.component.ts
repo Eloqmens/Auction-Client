573f442492cb740c86c830e35ee2ec290c376662
Eloqmens <mesutovmuhammed@gmail.com>
2024-08-13 03:45:02 +0300

    Pulling categories to create a lot

--- src/app/admin/create-lot/create-lot.component.ts
+++ src/app/admin/create-lot/create-lot.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LotService } from '../../services/lot.service';
 import { Lot } from '../../models/lot.model';
+import { Category } from '../../models/category.model';
 
 @Component({
   selector: 'app-create-lot',
   templateUrl: './create-lot.component.html',
   styleUrls: ['./create-lot.component.css']
 })
-export class CreateLotComponent {
+export class CreateLotComponent implements OnInit {
   lot: Lot = {
     id: 0,
     title: '',
@@ -20,7 +21,19 @@ export class CreateLotComponent {
     userId: ''
   };
 
+  categories: Category[] = [];
+
   constructor(private lotService: LotService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  loadCategories(): void {
+    this.lotService.getCategories().subscribe((categories) => {
+      this.categories = categories;
+    });
+  }
 
   createLot(): void {
     this.lotService.createLot(this.lot).subscribe(() => {
